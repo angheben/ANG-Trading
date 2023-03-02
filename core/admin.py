c5e9b235173ca2268359f722c3ab383b689c6186
Vitor Angheben <vitor_angheben@hotmail.com>
2023-03-01 21:23:16 -0300

    models edited admin edited HTML templates edited

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Services, Features, Testimonial, Employees
 
-# Register your models here.
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ('service', 'description', 'choices')
--- core/models.py
+++ core/models.py
@@ -1,5 +1,28 @@
 from django.db import models
+from stdimage.models import StdImageField
 
 
 class Employees(models.Model):
     pass
+
+
+class Testimonial(models.Model):
+    pass
+
+
+class Features(models.Model):
+    pass
+
+
+class Services(models.Model):
+    ICON_CHOICES = (
+        ('lni-cog', 'Gear'),
+        ('lni-stats-up', 'Graphic'),
+        ('lni-users', 'Users'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Rocket')
+    )
+    service = models.CharField(name='service', max_length=100)
+    description = models.CharField(name='description', max_length=100)
+    choices = models.CharField(name='choices', max_length=100, choices=ICON_CHOICES)
